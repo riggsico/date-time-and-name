c7334bf011dd1ab928a9e42733b1059ac5801090
riggsico <132190687+riggsico@users.noreply.github.com>
2023-11-13 14:44:49 +0800

    Add files via upload

---
+++ date_time.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        <input type="date" name="date"> 
+        <br>
+        <input type="radio" name="name" value="allan">allan
+        <input type="radio" name="name" value="rex">rex
+        <br>
+        <input type="radio" name="time" value="8:00am">8:00am
+        <input type="radio" name="time" value="9:00am">9:00am
+        <br>
+        <input type="submit">  
+    </form>   
+</body>
+</html>
+
